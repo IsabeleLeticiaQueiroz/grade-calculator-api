0683fe2fa97496c89f59b5bcd0b74a124aa3c553
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-07-03 23:30:46 -0300

    uPDATING CONNECTION

--- server.js
+++ server.js
@@ -25,11 +25,13 @@ app.use("/api/studyperiods", protect, studyPeriodRoutes);
 
 
 //mongodb connection
+const PORT = process.env.PORT || 5000; // 5000 é fallback local
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB conectado");
-    app.listen(process.env.PORT, () =>
-      console.log(`🚀 Servidor rodando na porta ${process.env.PORT}`)
+    app.listen(PORT, () =>
+      console.log(`🚀 Servidor rodando na porta ${PORT}`)
     );
   })
   .catch((err) => console.error("❌ Erro ao conectar:", err));
